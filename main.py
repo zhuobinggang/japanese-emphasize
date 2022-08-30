1e01fbdf03bc457aa37bf90b372867615f99d82e
zhuobinggang <zhuobinggang@gmail.com>
2022-08-30 12:42:13 +0900

    BUG修复

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ def label_to_number(case):
     numbers = [1 if label != 'O' else 0 for label in labels]
     return tokens, numbers
 
-def create_model_with_seed(seed):
+def create_model_with_seed(seed, cuda):
     t.manual_seed(seed)
     np.random.seed(seed)
-    m = Sector_2022()
+    m = Sector_2022(cuda = cuda)
     time_string = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(f'created model with seed {seed} at time {time_string}')
     return m
@@ -53,7 +53,7 @@ def flatten(l):
     return [item for sublist in l for item in sublist]
 
 class Sector_2022(nn.Module):
-  def __init__(self, learning_rate = 2e-5):
+  def __init__(self, learning_rate = 2e-5, cuda = False):
     super().__init__()
     self.learning_rate = learning_rate
     self.bert_size = 768
@@ -61,7 +61,9 @@ class Sector_2022(nn.Module):
     self.init_bert()
     self.init_hook()
     self.opter = t.optim.AdamW(self.get_should_update(), self.learning_rate)
-    # self.cuda()
+    if cuda:
+        self.cuda()
+    self.is_cuda = cuda
 
   def init_bert(self):
     self.bert = BertModel.from_pretrained('cl-tohoku/bert-base-japanese-char')
@@ -73,11 +75,9 @@ class Sector_2022(nn.Module):
 
   def init_hook(self): 
     self.classifier = nn.Sequential( # 因为要同时判断多种1[sep]3, 2[sep]2, 3[sep]1, 所以多加一点复杂度
-      nn.Linear(self.bert_size, 20),
+      nn.Linear(self.bert_size, 384),
       nn.LeakyReLU(0.1),
-      nn.Linear(20, 20),
-      nn.LeakyReLU(0.1),
-      nn.Linear(20, 1),
+      nn.Linear(384, 1),
       nn.Sigmoid()
     )
 
@@ -91,15 +91,18 @@ def focal_loss(o, l, fl_rate = 0):
     loss = (-1) * t.log(pt) * t.pow((1 - pt), fl_rate)
     return loss
 
-def test(m, ds_test):
+def test(m, ds_test_org):
+    ds_test = ds_test_org.copy()
     toker = m.toker
     bert = m.bert
     target_all = []
     result_all = []
     for text, labels in ds_test:
         ids = encode(text, toker)
-        # out_bert = bert(ids.unsqueeze(0).cuda()).last_hidden_state # (1, seq_len + 2, 768)
-        out_bert = bert(ids.unsqueeze(0)).last_hidden_state # (1, seq_len + 2, 768)
+        if m.is_cuda:
+            out_bert = bert(ids.unsqueeze(0).cuda()).last_hidden_state # (1, seq_len + 2, 768)
+        else:
+            out_bert = bert(ids.unsqueeze(0)).last_hidden_state # (1, seq_len + 2, 768)
         out_bert = out_bert[:, 1:-1, :] # (1, seq_len, 768)
         out_mlp = m.classifier(out_bert) # (1, seq_len, 1)
         out_mlp = out_mlp.squeeze() # (seq_len)
@@ -107,9 +110,9 @@ def test(m, ds_test):
         result_all.append(out_mlp.tolist())
     return result_all, target_all
 
-    
 
-def train(m, ds_train, epoch = 1, batch = 16, iteration_callback = None, random_seed = True):
+def train(m, ds_train_org, epoch = 1, batch = 16, iteration_callback = None, random_seed = True):
+    ds_train = ds_train_org.copy()
     first_time = datetime.datetime.now()
     toker = m.toker
     bert = m.bert
@@ -127,18 +130,15 @@ def train(m, ds_train, epoch = 1, batch = 16, iteration_callback = None, random_
                 print(f'finished: {row_idx}/{len(ds_train)}')
                 pass
             ids = encode(text, toker)
-            # out_bert = bert(ids.unsqueeze(0).cuda()).last_hidden_state # (1, seq_len + 2, 768)
-            out_bert = bert(ids.unsqueeze(0)).last_hidden_state # (1, seq_len + 2, 768)
+            if m.is_cuda:
+                out_bert = bert(ids.unsqueeze(0).cuda()).last_hidden_state # (1, seq_len + 2, 768)
+                labels = t.FloatTensor(labels).cuda() # (seq_len)
+            else:
+                out_bert = bert(ids.unsqueeze(0)).last_hidden_state # (1, seq_len + 2, 768)
+                labels = t.FloatTensor(labels) # (seq_len)
             out_bert = out_bert[:, 1:-1, :] # (1, seq_len, 768)
             out_mlp = m.classifier(out_bert) # (1, seq_len, 1)
             out_mlp = out_mlp[0, :, 0] # (seq_len)
-            # labels = t.FloatTensor(labels).cuda() # (seq_len)
-            labels = t.FloatTensor(labels) # (seq_len)
-            if labels.shape != out_mlp.shape:
-                print(row_idx)
-                print(ids)
-                print(text)
-                print(labels)
             loss = BCE(out_mlp, labels)
             loss.backward()
             # backward
@@ -189,15 +189,11 @@ def test_chain(m, ds_test):
     results, targets = get_results_and_targets(m, ds_test)
     return cal_prec_rec_f1_v2(results, targets)
 
-def get_m():
-    RANDOM_SEED = 2000
-    m = create_model_with_seed(RANDOM_SEED)
-    return m
-
-def run(m):
+def run():
     ds_train = read_train()
     ds_test = read_test()
     results = []
+    m = create_model_with_seed(20, cuda = True)
     for _ in range(5):
         train(m, ds_train, epoch = 1, batch = 16, iteration_callback = None, random_seed = True)
         result = test_chain(m, ds_test)
@@ -207,14 +203,14 @@ def run(m):
 
 RANDOM_SEEDs = [20, 22, 8, 29, 1648, 1,2]
     
-def experiment(epoch = 4):
+def experiment(epoch = 5, cuda = False):
     results_5X5 = []
     train_dss = read_trains()
     test_dss = read_tests()
     for _, (ds_train, ds_test) in enumerate(zip(train_dss, test_dss)):
         results = []
         for idx in range(5):
-            m = create_model_with_seed(RANDOM_SEEDs[idx])
+            m = create_model_with_seed(RANDOM_SEEDs[idx], cuda)
             for _ in range(epoch):
                 train(m, ds_train, epoch = 1, batch = 16, iteration_callback = None, random_seed = True)
             result = test_chain(m, ds_test)
